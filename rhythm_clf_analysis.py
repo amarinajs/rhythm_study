d2802ee346b9bedc1fb3bbe3af3a8c20085b3da3
Ana Marina <amarinajs@gmail.com>
2015-07-07 11:48:58 -0400

    fixed bug in finger mask, eliminated kNN clasifier, added searchlight and creation of a log file with the printed results, and saving results in a nifti file3 called fingers_sl_motor.gz

--- rhythm_clf_analysis.py
+++ rhythm_clf_analysis.py
@@ -11,48 +11,41 @@ Script to make the classification analysis """
 
 from mvpa2.tutorial_suite import *
 import os
-import glob 
 import sys
 import numpy as np
-import pylab
 
 
 
-verbose.level = 6
-
-sys.argv[1:]
+verbose.level = 5
 
 verbose(1, "Loading  h5 files")
+print "Loading  h5 files"
 for subject_dir in sys.argv[1:]:
+
+        former, sys.stdout = sys.stdout, open(os.path.join(os.getcwd(), subject_dir, 'log.txt'), 'w')
 
         filename = os.path.join(os.getcwd(), subject_dir, "dataset_%s.hdf5" % subject_dir)
         
         data = h5load(filename)
         verbose(2, "Creating dataset for response ")
-        fingers  = data[:, data.fa.si>0]
+        print "Creating dataset for response "
+        fingers  = data[:, data.fa.fi>0]
         response = fingers[fingers.targets == 'response']
         res_cor = response[response.sa.field_response_visual != 'none']
-        evds = dataset_wizard(res_cor.samples,targets=res_cor.sa.field_response_visual, chunks=res_cor.sa.chunks)
-        verbose(3, "Creating classifier")
-        clf = kNN(k=1,
-                  dfx=one_minus_correlation,
-                  voting='majority')
+        res_cor.targets = res_cor.sa.field_response_visual
+        #evds = dataset_wizard(res_cor.samples,targets=res_cor.sa.field_response_visual, chunks=res_cor.sa.chunks)
+        evds = res_cor
+        print evds.summary()
         verbose(3, "Starting Balancer")
-        Bal = Balancer(amount='equal', attr='targets', limit='chunks',apply_selection=True)
+        print "Starting Balancer"
+        Bal = Balancer(amount='equal', attr='targets', limit='chunks', apply_selection=True)
         evds = Bal(evds)
-        verbose(4, "Starting Crossvalidation with kNN")
-        cvte = CrossValidation(clf,
-                             NFoldPartitioner(),
-                             errorfx = lambda p,
-                             t: np.mean(p == t),
-                             #postproc=mean_sample(),
-                             enable_ca = ['stats'])
-        cv_results = cvte(evds)
-        verbose(5, "Printing Cross Validation results")
-        print cvte.ca.stats
+        print evds.summary()
         verbose(3, "Creating classifier")
+        print "Creating classifier"
         clf = LinearCSVMC()
         verbose(4, "Starting Crossvalidation with LinearCSVMC")
+        print "Starting Crossvalidation with LinearCSVMC"
         cvte = CrossValidation(clf,
                              NFoldPartitioner(),
                              errorfx = lambda p,
@@ -60,18 +53,18 @@ for subject_dir in sys.argv[1:]:
                              #postproc=mean_sample(),
                              enable_ca = ['stats'])
         cv_results = cvte(evds)
-        verbose(5, "Printing Cross Validation results")
+        verbose(4, "Starting SearchLight")
+        print "Starting SearchLight"
+        sl = sphere_searchlight(cvte, radius=3, postproc=mean_sample())
+        sl_results = sl(evds)
+        verbose(5, "Printing Cross Validation amd SearchLight results")
+        print "Printing Cross Validation amd SearchLight results"
         print cvte.ca.stats
+        print 'Best performing sphere error:', np.min(sl_results.samples)
+        map2nifti(evds, sl_results).to_filename(os.path.join(os.getcwd(), subject_dir, "fingers_sl_motor.gz"))
+        verbose(1, "saving log file")
+        print "saving log file"
+        results, sys.stdout = sys.stdout, former
+        results.close()
 
-        #sl = Searchlight(cvte,
-        #         IndexQueryEngine(voxel_indices=Sphere(1),
-        #                          event_offsetidx=Sphere(2)),
-        #         postproc=mean_sample())
-        #res = sl(evds)
-        #verbose(6, "Printing Searchlight results")
-        #print res
-        #print cvte.ca.stats.matrix
-        #cvte.ca.stats.plot(labels = ['left', 'right'])
-        #pylab.show()
-        #cvte = CrossValidation(GNB(), NFoldPartitioner(),
-            #                   postproc=mean_sample())
+
